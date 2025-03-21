f1ec9c6ffee97d0c13bc65504f014daac101e8db
Willigham <willigamcoelho@gmail.com>
2025-03-21 17:53:52 -0300

    Aula2

---
+++ Atividade1_Engage.py
@@ -0,0 +1,5 @@
+#Escreva um algoritmo que leia o valor da hora trabalhada de um funcionário e o número de horas trabalhadas no mês.  Calcule e mostre o total do seu salário no referido mês.
+
+x = int(input("Informe o valor da hora trabalhada:"))
+y = int(input("Informe a quantidade de horas trabalhadas no mês:"))
+print("O salário a receber é:", x * y)
---
+++ Atividade2_Engage.py
@@ -0,0 +1,5 @@
+#atividade engage 2
+a = int(input("Informe o valor de A:"))
+c = int(input("Informe o valor de C:"))
+b = (a + c) / 2
+print("O valor de B:", b)
